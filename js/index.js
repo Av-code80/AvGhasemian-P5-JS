715d59922c68b9766aa7287c8125b650d1bba68e
avehghasemian <av.ghasemian@gmail.com>
2021-02-03 19:13:35 +0100

    foncDisplayAppareil

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 /* Récupération des données depuis l'API 
     La méthode affichageCarte() est définie dans ClassCarteProduit.js
 */
@@ -7,10 +8,12 @@ const fetchApi = async function () {
     const jsonData = await response.json();
     displayAppareil(jsonData);
 } 
-//On crée une fonction qui va créer une carte produit pour chaque élément renvoyé dans le JSON de l'API
+//On crée une fonction qui va créer une carte produit 
+//pour chaque élément renvoyé dans le JSON de l'API
 function displayAppareil(data) {
     data.forEach(element => {
-        const blocCarteAppareil = new Carte(element.lenses, element._id, element.name, element.price, element.description, element.imageUrl);
+        const blocCarteAppareil = new Carte(element.lenses, element._id, element.name, 
+            element.price, element.description, element.imageUrl);
         blocCarteAppareil.affichageCarte();     
     });      
 }
