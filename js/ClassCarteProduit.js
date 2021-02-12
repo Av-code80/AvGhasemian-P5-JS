5db24cf1d73a71dc84e37802e8811fd6a454dc7b
avehghasemian <av.ghasemian@gmail.com>
2021-02-12 23:56:21 +0100

    classCarte

--- js/ClassCarteProduit.js
+++ js/ClassCarteProduit.js
@@ -20,16 +20,35 @@ class Carte {
 
         // Construction de l'élément carte
         const wrapper = document.getElementById('wrapper');
-        const carte = document.createElement('article');carte.classList.add('carte');carte.setAttribute("id", this._id);
-        const carteImage = document.createElement('img');carteImage.classList.add('carte__image');carteImage.setAttribute("src", this.imageUrl);
-        const cartePrix = document.createElement('div');cartePrix.classList.add('carte__prix');cartePrix.textContent = this.displayPrice('fr-FR', 'EUR');
-        const carteDescription = document.createElement('div');carteDescription.classList.add('carte__description');
-        const carteDescriptionHover = document.createElement('div');carteDescriptionHover.classList.add('carte__description-hover');carteDescriptionHover.textContent = this.description;
-        const carteTitre = document.createElement('h2'); carteTitre.classList.add('carte__description__titre');carteTitre.textContent = this.name;
-        const carteLenses = document.createElement('ul'); carteLenses.classList.add('carte__description__option');
+
+        const carte = document.createElement('article');
+        carte.classList.add('carte');
+        carte.setAttribute("id", this._id);
+
+        const carteImage = document.createElement('img');
+        carteImage.classList.add('carte__image');
+        carteImage.setAttribute("src", this.imageUrl);
+        
+        const cartePrix = document.createElement('div');
+        cartePrix.classList.add('carte__prix');
+        cartePrix.textContent = this.displayPrice('fr-FR', 'EUR');
+
+        const carteDescription = document.createElement('div');
+        carteDescription.classList.add('carte__description');
+
+        const carteDescriptionHover = document.createElement('div');
+        carteDescriptionHover.classList.add('carte__description-hover');carteDescriptionHover.textContent = this.description;
+
+        const carteTitre = document.createElement('h2'); 
+        carteTitre.classList.add('carte__description__titre');
+        carteTitre.textContent = this.name;
+
+        const carteLenses = document.createElement('ul'); 
+        carteLenses.classList.add('carte__description__option');
         
         this.lenses.forEach(lense => {
-            const carteLensesItems = document.createElement('li');carteLensesItems.textContent = lense; 
+            const carteLensesItems = document.createElement('li');
+            carteLensesItems.textContent = lense; 
             carteLenses.appendChild(carteLensesItems); 
         });
         
