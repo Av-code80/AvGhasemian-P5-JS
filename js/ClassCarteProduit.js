eceef7eb7c7c5fb288fd64ccdc7cb0bc5a7a18ea
avehghasemian <av.ghasemian@gmail.com>
2021-02-18 13:12:41 +0100

    erreur modification

--- js/ClassCarteProduit.js
+++ js/ClassCarteProduit.js
@@ -1,5 +1,4 @@
 /* Création de la classe carte pour la page Index
-
     affichageCarte() 
     => a pour but de créer les différents élements, de les assigner dans le DOM
     => a pour but de générer une URL à partir des éléments de la carte cliquée
@@ -15,57 +14,32 @@ class Carte {
         this.imageUrl = imageUrl
     }
 
-
     affichageCarte(){
 
         // Construction de l'élément carte
         const wrapper = document.getElementById('wrapper');
-
-        const carte = document.createElement('article');
-        carte.classList.add('carte');
-        carte.setAttribute("id", this._id);
-
-        const carteImage = document.createElement('img');
-        carteImage.classList.add('carte__image');
-        carteImage.setAttribute("src", this.imageUrl);
-        
-        const cartePrix = document.createElement('div');
-        cartePrix.classList.add('carte__prix');
-        cartePrix.textContent = this.displayPrice('fr-FR', 'EUR');
-
-        const carteDescription = document.createElement('div');
-        carteDescription.classList.add('carte__description');
-
-        const carteDescriptionHover = document.createElement('div');
-        carteDescriptionHover.classList.add('carte__description-hover');
-        carteDescriptionHover.textContent = this.description;
-
-        const carteTitre = document.createElement('h2'); 
-        carteTitre.classList.add('carte__description__titre');
-        carteTitre.textContent = this.name;
-
-        const carteLenses = document.createElement('ul'); 
-        carteLenses.classList.add('carte__description__option');
+        const carte = document.createElement('article');carte.classList.add('carte');carte.setAttribute("id", this._id);
+        const carteImage = document.createElement('img');carteImage.classList.add('carte__image');carteImage.setAttribute("src", this.imageUrl);
+        const cartePrix = document.createElement('div');cartePrix.classList.add('carte__prix');cartePrix.textContent = this.displayPrice('fr-FR', 'EUR');
+        const carteDescription = document.createElement('div');carteDescription.classList.add('carte__description');
+        const carteDescriptionHover = document.createElement('div');carteDescriptionHover.classList.add('carte__description-hover');carteDescriptionHover.textContent = this.description;
+        const carteTitre = document.createElement('h2'); carteTitre.classList.add('carte__description__titre');carteTitre.textContent = this.name;
+        const carteLenses = document.createElement('ul'); carteLenses.classList.add('carte__description__option');
         
         this.lenses.forEach(lense => {
-            const carteLensesItems = document.createElement('li');
-            carteLensesItems.textContent = lense; 
+            const carteLensesItems = document.createElement('li');carteLensesItems.textContent = lense; 
             carteLenses.appendChild(carteLensesItems); 
         });
         
-
         // Assignation des éléments 
         wrapper.appendChild(carte);
-        carte.appendChild(carteImage);
-        carte.appendChild(cartePrix);
-        carte.appendChild(carteDescription);
-        
+        carte.appendChild(carteImage);carte.appendChild(cartePrix);carte.appendChild(carteDescription);
         carteDescription.appendChild(carteTitre);
         carteDescription.appendChild(carteLenses);
         carte.appendChild(carteDescriptionHover);
         
         // Création automatique de l'URL de destination qui prend en compte l'Id de chaque produit.
-        const urlElement = new URL("https://av-code80.github.io/AvGhasemian-P5-JS/produit.html");
+        const urlElement = new URL("https://thomasgouin.github.io/projet5-oc-Orinoco/produit.html");
             urlElement.searchParams.set("id", this._id);
             
         // Au clic sur chaque carte, nous renvoyons vers l'URL correspondante au produit
@@ -78,4 +52,3 @@ class Carte {
         return Intl.NumberFormat(lang, { style: 'currency', currency: money }).format(this.price/100)
     }
 }
-
--- js/produit.js
+++ js/produit.js
@@ -2,7 +2,7 @@
 //Création de la requete fetch asynchrone, on attend la réponse puis on appelle le JSON de la réponse.
 const myUrl = new URL(window.location.href);
 const fetchApi = async function () {
-    const response = await fetch (''+ myUrl.searchParams.get("id"))
+    const response = await fetch ('https://oc-p5-api.herokuapp.com/api/cameras/'+ myUrl.searchParams.get("id"))
     const jsonData = await response.json();
 
     //On sotcke les éléments du fichier JSON dans une variable.
@@ -72,4 +72,3 @@ const fetchApi = async function () {
 } 
 //On appelle notre fonction
 fetchApi();
-
